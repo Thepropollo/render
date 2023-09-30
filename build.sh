637bf6775ab78f029f68a64efe2e1ba7a2be8dc2
Thepropollon <e1316836327@live.uleam.edu.ec>
2023-09-30 09:18:28 -0500

    First Commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
+
+python manage.py collectstatic --no-input
+python manage.py migrate
